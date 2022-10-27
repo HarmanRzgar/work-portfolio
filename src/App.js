5249f54f200aef42b601172d6c210173b8edf65a
HarmanRzgar <108234752+HarmanRzgar@users.noreply.github.com>
2022-10-27 21:30:16 +0300

    Resized some of the fonts and added some text

--- src/App.js
+++ src/App.js
@@ -9,26 +9,28 @@ import { SiJavascript, SiHtml5, SiCss3, SiJava, SiReact, SiRedux, SiReactrouter,
 
 function App() {
 
-  let socials = [ {icon : <SiGithub/> , Link : 'https://github.com/HarmanRzgar'},
-                  {icon : <SiLinkedin/> , Link : 'https://www.linkedin.com/in/harman-rzgar-muhammad-9b186a23b/'},
-                  {icon : <SiGmail/> , Link : 'https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=harmanrzgarmuhammad@gmail.com'}
+  let socials = [
+   {icon : <SiGithub/> , Link : 'https://github.com/HarmanRzgar'},
+   {icon : <SiLinkedin/> , Link : 'https://www.linkedin.com/in/harman-rzgar-muhammad-9b186a23b/'},
+   {icon : <SiGmail/> , Link : 'https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=harmanrzgarmuhammad@gmail.com'}
 ];
 
-let sidebaricons = [ {icon : <BsFillPersonFill/>, Link : "#About" },
-                     {icon : <BiNetworkChart/> , Link : "#Skills"},
-                     {icon : <BsCodeSquare/> , Link : "#Projects"},
-                     {icon : <MdAlternateEmail/>, Link : "#Contact" }
-]
+let sidebaricons = [ 
+  {icon : <BsFillPersonFill/>, Link : "#About" },
+  {icon : <BiNetworkChart/> , Link : "#Skills"},
+  {icon : <BsCodeSquare/> , Link : "#Projects"},
+  {icon : <MdAlternateEmail/>, Link : "#Contact" }
+];
 
 let listSidebarIcons = sidebaricons.map((icons) =>
           <li><a href={icons.Link}>{icons.icon}</a></li>
 );
 
 let programming = [ 
-                     {title : 'Java', icon : <SiJava/> ,  color : 'color-java'},
-                     {title : 'C++', icon : <SiCplusplus/> ,  color : 'color-cpp'},
-                     {title : 'JavaScript', icon : <SiJavascript/>,  color : 'color-javascript'}
-]
+  {title : 'Java', icon : <SiJava/> ,  color : 'color-java'},
+  {title : 'C++', icon : <SiCplusplus/> ,  color : 'color-cpp'},
+  {title : 'Javascript', icon : <SiJavascript/>,  color : 'color-javascript'}
+];
 
 let webnDesign = [ 
   {title : 'HTML', icon : <SiHtml5/> ,  color : 'color-html'},
@@ -38,7 +40,7 @@ let webnDesign = [
   {title : 'React', icon : <SiReact/> ,  color : 'color-react'},
   {title : 'Photoshop', icon : <SiAdobephotoshop/>,  color : 'color-photoshop'},
   {title : 'Adobe XD', icon : <SiAdobexd/> ,  color : 'color-adobeXD'},
-]
+];
 
 let contacts = socials.map((social) => 
 <a className="contact-card"
@@ -84,39 +86,41 @@ const listDesign = webnDesign.map((list) =>
     <div className="body">
     <div className="home" id='#'>
     <h1>Hello folks! I am <span className='Typed'>Harman Rzgar</span></h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa totam dolorum, vel enim, debitis molestias illum similique labore voluptatibus reiciendis cumque eaque? Qui culpa suscipit minus impedit accusantium similique veritatis?</p>
+    <p>I am a Junior Front-End developer and progammer from Kurdistan. </p>
+    <div className="button">My Resume</div>
    </div>
    <div className="About" id='About'>
     <h1>About Me</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa totam dolorum, vel enim, debitis molestias illum similique labore voluptatibus reiciendis cumque eaque? Qui culpa suscipit minus impedit accusantium similique veritatis?</p>
-
+    <p>Residing in Erbil/KRI, I am a Web developer, techie and a sustainability enthusiast. I am an ISE Undergrad at EPU and a WeCode Front-End Development Bootcamp Grad of summer 2022.</p>
    </div>
    <div className="Skills" id='Skills'>
     <h1>My Skillsets</h1> 
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa totam dolorum, vel enim, debitis molestias illum similique labore voluptatibus reiciendis cumque eaque? Qui culpa suscipit minus impedit accusantium similique veritatis?</p>
-   <h1 className='skills-top'>Programming</h1> 
+    <p>I have strong interest in various fields and Programming and Design are on the top among them.</p>
+   <h2 className='skills-top'>Programming</h2> 
    <div className="skills-programming">
             {listProgramming}
         </div>
 
-       <h1 className='skills-top'> Web Development & Design</h1>
+       <h2 className='skills-top'> Web Development & Design</h2>
         <div className="skills-show">
             {listDesign}
     </div>
 
    </div>
    <div className="Projects" id='Projects'>
-    <h1>My work</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa totam dolorum, vel enim, debitis molestias illum similique labore voluptatibus reiciendis cumque eaque? Qui culpa suscipit minus impedit accusantium similique veritatis?</p>
+    <h1>Work</h1>
+    <p></p>
+    <h2>Projects</h2>
+
+    <h2>Experience</h2>
 
    </div>
    <div className="Contact" id='Contact'>
     <h1>Contact me</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa totam dolorum, vel enim, debitis molestias illum similique labore voluptatibus reiciendis cumque eaque? Qui culpa suscipit minus impedit accusantium similique veritatis?</p>
+    <p>Have a project in mind? Please reach out via my socials below!</p>
+    <br />
       <div className="contact-content">
-          
           {contacts}
-
       </div>
    </div>
    </div>
