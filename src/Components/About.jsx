26215ea51e4ec2bea66461cc8ab5f327c828aaec
HarmanRzgar <108234752+HarmanRzgar@users.noreply.github.com>
2023-01-01 09:57:11 +0300

    Eslint updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,7 +6,7 @@ function About() {
     <div className="About" id='About'>
     <h1>About Me</h1>
     <p>Residing in Erbil/KRI, I am a junior Web developer, techie and a sustainability enthusiast. I am an 
-      <span><a href="https://epu.edu.iq/erbil-technical-engineering-college/information-systems-engineering-department/">ISE Undergrad at EPU</a></span>
+      <span><a href="https://epu.edu.iq/erbil-technical-engineering-college/information-systems-engineering-department/"> ISE Undergrad at EPU </a></span>
        and a <span><a href="https://www.linkedin.com/posts/harman-rzgar-muhammad-9b186a23b_wecodeabrrwanga-activity-6980916092663676928-EdSB?utm_source=share&utm_medium=member_desktop">
         WeCode Front-End Development Bootcamp</a></span> Grad of summer 2022.</p>
    </div>
