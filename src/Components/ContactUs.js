f45ed464fc819ecc78ee9d4203b8c7c6b9ce9503
HarmanRzgar <108234752+HarmanRzgar@users.noreply.github.com>
2023-01-03 23:58:59 +0300

    updated versions

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,8 +1,11 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import '../App.css'
+import { useState } from 'react';
+
 
 export const ContactUs = () => {
+  
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -21,7 +24,8 @@ export const ContactUs = () => {
       <label>Name</label>
       <input type="text" name="user_name" className='contact-input' />
       <label>Email</label>
-      <input type="email" name="user_email" className='contact-input'/>
+      <input type="email" name="user_email" className='contact-input' id='email'/>
+      <p id='result'></p>
       <label>Message</label>
       <textarea name="message" className='contact-text'/>
       <input type="submit" value="Send" className='button' id='button-form'/>
